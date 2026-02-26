8bcef1791ce1a7662d6425fe1744e6cf1ac3534f
kapasiraj84-beep <kapasiraj84@gmail.com>
2026-02-26 16:14:17 +0530

    🗺️ TEMP: Script to add Google Maps links to location addresses

---
+++ location-link-update.js
@@ -0,0 +1,15 @@
+// This script will be used to update index.html with clickable Google Maps links
+// Google Maps URL for Bhavya Steel Industries location
+
+const GOOGLE_MAPS_URL = 'https://www.google.com/maps/dir/?api=1&destination=23.0910902,72.6620856&destination_place_id=ChIJOPqJqJCJXjkRqPqJqJCJXjk';
+
+// Alternative: Direct Google Maps link
+const GOOGLE_MAPS_DIRECT = 'https://maps.google.com/?q=23.0910902,72.6620856';
+
+// Alternative: Search-based link
+const GOOGLE_MAPS_SEARCH = 'https://www.google.com/maps/search/?api=1&query=Satguru+Swami+Teoonramji+Maharaj+Overbridge+Naroda+Ahmedabad';
+
+console.log('Google Maps URLs for Bhavya Steel Industries:');
+console.log('Directions URL:', GOOGLE_MAPS_URL);
+console.log('Direct URL:', GOOGLE_MAPS_DIRECT);
+console.log('Search URL:', GOOGLE_MAPS_SEARCH);
